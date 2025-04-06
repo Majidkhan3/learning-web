2bcb47cd5f9431187d2f204570c41960d350d2e9
Majidkhan3 <majidabid017@gmail.com>
2025-04-06 23:03:37 +0500

    chnages

--- src/components/LogoBox.jsx
+++ src/components/LogoBox.jsx
@@ -6,11 +6,11 @@ import Link from 'next/link';
 import Language from "@/assets/images/language.png"
 const LogoBox = () => {
   return <div className="logo-box">
-      <Link href="/dashboards/analytics" className="logo-dark">
+      <Link href="/dashboards/espagnol" className="logo-dark">
         <Image width={28} height={28} src={Language} className="logo-sm" alt="logo sm" />
         <Image width={28} height={30} src={Language} className="logo-lg" alt="logo dark" />
       </Link>
-      <Link href="/dashboards/analytics" className="logo-light">
+      <Link href="/dashboards/espagnol" className="logo-light">
         <Image width={28} height={28} src={Language} className="logo-sm" alt="logo sm" />
         <Image width={28} height={30} src={Language} className="logo-lg" alt="logo light" />
       </Link>
--- src/components/layout/TopNavigationBar/page.jsx
+++ src/components/layout/TopNavigationBar/page.jsx
@@ -25,7 +25,7 @@ const page = () => {
 
               <MaximizeScreen />
 
-              <Notifications />
+              {/* <Notifications /> */}
 
               <ThemeCustomizerToggle />
 
